1cf74d65d12970f1c44b92e3a23f2267f600ec9e
unknown <smartdesk2015@gmail.com>
2023-08-31 20:26:34 +0530

    chatterease_socket

---
+++ index.js
@@ -0,0 +1,51 @@
+import { Server } from 'socket.io';
+
+const io = new Server(9000, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }, 
+})
+
+// server link  --- abcdd 
+// socket -=== bcdf  
+let users = [];
+
+const addUser = (userData, socketId) => {
+    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.sub === userId);
+}
+
+io.on('connection',  (socket) => {
+    console.log('user connected')
+
+    //connect
+    socket.on("addUser", userData => {
+        addUser(userData, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    //send message
+    socket.on('sendMessage', (data) => {
+        const user = getUser(data.receiverId);
+        if (user) {
+            io.to(user.socketId).emit('getMessage', data);
+        } else {
+            // Handle the case where the user is not found
+            console.log('User not found.');
+        }
+    });
+    
+    //disconnect
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        removeUser(socket.id);
+        io.emit('getUsers', users);
+    })
+})
