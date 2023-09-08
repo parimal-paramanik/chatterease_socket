8ef1709410ac3959bb7c04fa45d3567e93469862
unknown <smartdesk2015@gmail.com>
2023-09-08 14:03:15 +0530

    frontend link

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 
 const io = new Server(9000, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://chatterease.netlify.app/',
     }, 
 })
 
